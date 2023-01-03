1f6b595c446af4b4e8b38e2ab3fd5ea9478f7b08
TalkBog <99033517+TalkBog@users.noreply.github.com>
2023-01-03 16:49:08 +0100

    CSS ajout projet

--- MVC/LetsCode/src/View/CreationProjet.php
+++ MVC/LetsCode/src/View/CreationProjet.php
@@ -80,132 +80,132 @@
                 </div>
             </div>
 
-            <button id="ajouter" type="submit" >Ajouter</button>
-            <a id="annuler" href="http://localhost" >Annuler</a>
-            <!--        --><?php //if(isset($_SESSION['roles']) && $_SESSION['roles'] ==  'Premium_User') : ?>
-            <!--        <div>-->
-            <!--            <p>Est-ce que votre projet est premium ?</p>-->
-            <!--            <input type="radio" id="oui" name="isPremium" value="oui"-->
-            <!--            <label for="oui">Oui</label>-->
-            <!--            <input type="radio" id="non" name="isPremium" value="non" checked>-->
-            <!--            <label for="non">Non</label>-->
-            <!--        </div>-->
-            <!--        --><?php //endif; ?>
-            <!--        <div class="tags">-->
-            <!--            <fieldset>-->
-            <!--                <legend>Langage de programmation</legend>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=36>-->
-            <!--                    <label for="tag[]">HTML</label>-->
-            <!--                </div>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=6>-->
-            <!--                    <label for="tag[]">Java</label>-->
-            <!--                </div>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=3>-->
-            <!--                    <label for="tag[]">Python</label>-->
-            <!--                </div>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=1>-->
-            <!--                    <label for="tag[]">C++</label>-->
-            <!--                </div>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=4>-->
-            <!--                    <label for="tag[]">C</label>-->
-            <!--                </div>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=5>-->
-            <!--                    <label for="tag[]">C#</label>-->
-            <!--                </div>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=2>-->
-            <!--                    <label for="tag[]">PHP</label>-->
-            <!--                </div>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=37>-->
-            <!--                    <label for="tag[]">CSS</label>-->
-            <!--                </div>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=38>-->
-            <!--                    <label for="tag[]">SQL</label>-->
-            <!--                </div>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=7>-->
-            <!--                    <label for="tag[]">Javascript</label>-->
-            <!--                </div>-->
-            <!---->
-            <!--            </fieldset>-->
-            <!--        </div>-->
-            <!---->
-            <!--        <div class="tags">-->
-            <!--            <fieldset>-->
-            <!--                <legend>Support</legend>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=33>-->
-            <!--                    <label for="tag[]">Windows</label>-->
-            <!--                </div>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=34>-->
-            <!--                    <label for="tag[]">Linux</label>-->
-            <!--                </div>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=35>-->
-            <!--                    <label for="tag[]">Mac</label>-->
-            <!--                </div>-->
-            <!--            </fieldset>-->
-            <!--        </div>-->
-            <!--        <div class="tags">-->
-            <!--            <fieldset>-->
-            <!--                <legend>Langue</legend>-->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=29>-->
-            <!--                    <label for="tag[]">Français</label>-->
-            <!--                </div>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=30>-->
-            <!--                    <label for="tag[]">English</label>-->
-            <!--                </div>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=31>-->
-            <!--                    <label for="tag[]">Español</label>-->
-            <!--                </div>-->
-            <!---->
-            <!--                <div>-->
-            <!--                    <input type="checkbox" id="tag[]" name="tag[]" value=32>-->
-            <!--                    <label for="tag[]">Deutsch</label>-->
-            <!--                </div>-->
-            <!--            </fieldset>-->
-            <!--        </div>-->
-            <!--        <div>-->
-            <!--            <select name="difficulte" id="difficulte" >-->
-            <!--                <option value="" disabled selected>Quel est la difficulté du projet ?</option>-->
-            <!--                <option value="facile">Facile</option>-->
-            <!--                <option value="moyen">Moyen</option>-->
-            <!--                <option value="difficile">Difficile</option>-->
-            <!--            </select>-->
-            <!--        </div>-->
-        </form>
+        </div>
+        <br>
+        <div class="flex-horizontal2">
+            <select name="difficulte" id="difficulte" class="input">
+                <option value="" disabled selected>Quel est la difficulté du projet ?</option>
+                <option value="facile">Facile</option>
+                <option value="moyen">Moyen</option>
+                <option value="difficile">Difficile</option>
+            </select>
 
-    </main>
+        </div>
+        <br>
+        <button id="ajouter" type="submit" >Ajouter</button>
+        <a id="annuler" href="http://localhost" >Annuler</a>
+                <?php if(isset($_SESSION['roles']) && $_SESSION['roles'] ==  'Premium_User') : ?>
+                <div>
+                    <p>Est-ce que votre projet est premium ?</p>
+                    <input type="radio" id="oui" name="isPremium" value="oui"
+                    <label for="oui">Oui</label>
+                    <input type="radio" id="non" name="isPremium" value="non" checked>
+                    <label for="non">Non</label>
+                </div>
+                <?php endif; ?>
+                <div class="tags">
+                    <fieldset>
+                        <legend>Langage de programmation</legend>
 
-    <?php include 'footer.php'?>
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=36>
+                            <label for="tag[]">HTML</label>
+                        </div>
 
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=6>
+                            <label for="tag[]">Java</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=3>
+                            <label for="tag[]">Python</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=1>
+                            <label for="tag[]">C++</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=4>
+                            <label for="tag[]">C</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=5>
+                            <label for="tag[]">C#</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=2>
+                            <label for="tag[]">PHP</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=37>
+                            <label for="tag[]">CSS</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=38>
+                            <label for="tag[]">SQL</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=7>
+                            <label for="tag[]">Javascript</label>
+                        </div>
+
+                    </fieldset>
+                </div>
+
+                <div class="tags">
+                    <fieldset>
+                        <legend>Support</legend>
+
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=33>
+                            <label for="tag[]">Windows</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=34>
+                            <label for="tag[]">Linux</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=35>
+                            <label for="tag[]">Mac</label>
+                        </div>
+                    </fieldset>
+                </div>
+                <div class="tags">
+                    <fieldset>
+                        <legend>Langue</legend>
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=29>
+                            <label for="tag[]">Français</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=30>
+                            <label for="tag[]">English</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=31>
+                            <label for="tag[]">Español</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="tag[]" name="tag[]" value=32>
+                            <label for="tag[]">Deutsch</label>
+                        </div>
+                    </fieldset>
+                </div>
+
+    </form>
     <script>var simplemde = new SimpleMDE({ element: document.querySelector("#contenu"), toolbar: false, toolbarTips: false, status:false });</script>
 </body>
 
