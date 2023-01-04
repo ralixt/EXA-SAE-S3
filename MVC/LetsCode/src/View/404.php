8d652636ef952e0a540a3c8721fc3fcb908eb5ef
ralixt <98614258+ralixt@users.noreply.github.com>
2023-01-04 12:53:06 +0100

    css filter

--- MVC/LetsCode/src/View/404.php
+++ MVC/LetsCode/src/View/404.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-    <link rel='stylesheet' href='../Ressource/css/accueil.css' type='text/css' media='screen'>
+    <link rel='stylesheet' href='../Ressource/css/404.css' type='text/css' media='screen'>
     <link rel='stylesheet' href='../Ressource/css/footer.css'  type='text/css' media='screen'>
     <link rel='stylesheet' href='../Ressource/css/header.css'  type='text/css' media='screen'>
     <title>Erreur 404</title>
@@ -14,7 +14,16 @@
 <body>
     <?php include 'header.php'?>
 
-    <h1>ERREUR 404</h1>
+    <main>
+        <div id="error">
+            <img src="../Ressource/images/rick.gif" alt="Rick">
+            <h1>ERREUR 404</h1>
+        </div>
+
+    </main>
+
+    <?php include 'footer.php'?>
+
 
 </body>
 
