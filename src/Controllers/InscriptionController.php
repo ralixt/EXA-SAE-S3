302e84314e5fee8b324b8c6c695d0558dc07cb0f
MohamedREHAIL <98965484+MohamedREHAIL@users.noreply.github.com>
2023-01-11 16:34:05 +0100

    login

--- src/Controllers/InscriptionController.php
+++ src/Controllers/InscriptionController.php
@@ -5,15 +5,14 @@ class InscriptionController extends AbstractController
 
     private CompteService $InscriptionService;
     private bool $c;
-    private bool $p;
-    private bool $e;
+    private bool $pseudo;
+    private bool $email;
     private string $erreur;
     public function __construct(AllService $Service) {
         parent::__construct(($Service));
         $this->InscriptionService=CompteService::getInstance();
         $this->c=false;
-        $this->p=true;
-        $this->e=true;
+
         $this->erreur="";
 
 
@@ -24,30 +23,11 @@ class InscriptionController extends AbstractController
     {
         $tabMPPseudo = $this->InscriptionService->getEmailMp();
         if(isset($_POST["confirmation_mp"])&&isset($_POST["creation_mp"])&&isset($_POST["adr_email"])&&isset($_POST["Pseudo"])) {
-                  for($i=0;$i<count($tabMPPseudo);$i++) {
-                      if ($tabMPPseudo[$i]["Pseudo"] == $_POST["Pseudo"] ) {
-                        $this->p=false;
-                      }
-                      elseif($tabMPPseudo[$i]["Pseudo"] != $_POST["Pseudo"]){
-                          $this->p=true;
-                      }
-                      if($tabMPPseudo[$i]["email"] == $_POST["adr_email"]){
-                          $this->e=false;
-                      }
-                      elseif($tabMPPseudo[$i]["email"] != $_POST["adr_email"]){
-                          $this->e=true;
-                      }
-                      if ($tabMPPseudo[$i]["Pseudo"] == $_POST["Pseudo"] and $tabMPPseudo[$i]["email"] == $_POST["adr_email"] ) {
-                          $this->p=false;
-                          $this->e=false;
-                      }elseif($tabMPPseudo[$i]["Pseudo"] != $_POST["Pseudo"] and $tabMPPseudo[$i]["email"] != $_POST["adr_email"]){
-                          $this->p=true;
-                          $this->e=true;
-                      }
-                  }
 
+               $pseudoAvailable= $this->InscriptionService->isPseudoAvailable($_POST["Pseudo"]);
+               $emailAvailable=$this->InscriptionService->isEmailAvailable($_POST["adr_email"]);
             //var_dump($this->InscriptionService->getEmailMp());
-                    if($this->p==true and $this->e==true) {
+                    if($pseudoAvailable and $emailAvailable) {
 
                         if (hash('sha256', $_POST["creation_mp"]) === hash('sha256', $_POST["confirmation_mp"])) {
                             $user = new User();
@@ -66,12 +46,12 @@ class InscriptionController extends AbstractController
                             $this->erreur = "les deux mots de passes ne sont pas identiques";
                         }
                     }
-                    elseif($this->p==false and $this->e==false ){
+                    elseif(!$pseudoAvailable and !$emailAvailable ){
                         $this->erreur = "l'adresse mail et le pseudo existe déjà";
                     }
-                    elseif($this->p==false){
+                    elseif(!$pseudoAvailable){
                         $this->erreur = "le Pseudo existe déjà";
-                    }elseif($this->e==false){
+                    }elseif(!$emailAvailable){
                         $this->erreur = "l'adresse mail existe déjà";
                     }
 
